fc9448d5986f0c58e49c58d3e6d0a41d1397101a
willitech <williamnnamani@yahoo.com>
2022-02-24 09:36:58 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
+import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  ,
   document.getElementById('root')
 );
 
